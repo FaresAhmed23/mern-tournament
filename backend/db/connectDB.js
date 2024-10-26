510ce2962ef46b6948091534a24e04c96a384026
FaresAhmed23 <153539187+FaresAhmed23@users.noreply.github.com>
2024-10-26 09:16:32 +0300

    Update connectDB.js

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI)
+        const conn = await mongoose.connect('mongodb+srv://fero:xGhdnKjSk8Sv6stC@cluster0.ss8ad.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0')
         console.log(`MongoDb Connected: ${conn.connection.host}`)
     } catch(err) {
         console.error(err)
