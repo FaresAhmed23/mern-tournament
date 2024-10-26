f3ffb22ae1b6e398afa3d8b41a84f641c83750a0
FaresAhmed23 <153539187+FaresAhmed23@users.noreply.github.com>
2024-10-26 09:20:27 +0300

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -4,7 +4,7 @@ import { User } from "../models/User.js";
 export const authMiddleware = async (req, res, next) => {
 	try {
 		const token = req.header("Authorization").replace("Bearer ", "");
-		const decoded = jwt.verify(token, process.env.JWT_SECRET);
+		const decoded = jwt.verify(token, 'secret');
 		const user = await User.findById(decoded.userId);
 
 		if (!user) {
